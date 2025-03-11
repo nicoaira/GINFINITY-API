db2d9344d85b85f9a1e759c82184b0a8ee27f600
Nicolas <aira.nicolas94@gmail.com>
2025-03-11 16:19:03 +0100

    gets db settings

--- config/settings.py
+++ config/settings.py
@@ -6,3 +6,13 @@ MODEL_PATH = os.getenv("MODEL_PATH", "models/model_weights.pth")
 # AWS placeholders (for future use)
 AWS_BUCKET_NAME = os.getenv("AWS_BUCKET_NAME", None)  # None means ignore AWS
 AWS_MODEL_KEY = os.getenv("AWS_MODEL_KEY", "model_weights.pth")
+
+# Example for local MySQL database (adjust these values as needed)
+DATABASE_HOST = os.getenv("DATABASE_HOST", "localhost")
+DATABASE_PORT = os.getenv("DATABASE_PORT", "3306")
+DATABASE_USER = os.getenv("DATABASE_USER",  default=None)
+DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD", default=None)
+DATABASE_NAME = os.getenv("DATABASE_NAME", "rna_db")
+
+# Construct the connection URL for MySQL (using pymysql)
+DATABASE_URL = f"mysql+pymysql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
